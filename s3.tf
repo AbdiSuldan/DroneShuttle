a24685d0f019ddb65dc9148b73d285ee687f8169
Abdi Suldan <Abdikarim.Suldan-cic@ibm.com>
2022-04-04 12:44:22 +0100

    try

--- s3.tf
+++ s3.tf
@@ -6,3 +6,11 @@ resource "aws_s3_bucket" "pipe_artifacts23" {
     Environment = "Dev"
   }
 }
+resource "aws_s3_bucket" "pipe_123testing" {
+  bucket = "just a test"
+
+  tags = {
+    Name        = "buckets for pipelines"
+    Environment = "Dev"
+  }
+}
