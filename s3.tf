59fc8de7396f11c7c94fb999cb0a35133c3a4529
Abdi Suldan <Abdikarim.Suldan-cic@ibm.com>
2022-04-04 12:40:37 +0100

    come on man

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "pipe_artifacts23" {
   bucket = "my-tf-pipeline-abdi23"
 
   tags = {
-    Name        = "buckets for pipeline"
+    Name        = "buckets for pipelines"
     Environment = "Dev"
   }
 }
